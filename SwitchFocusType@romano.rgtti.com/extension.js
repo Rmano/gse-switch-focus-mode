c6a6c3643b3300840f725fe85b7df65f2be6deb6
Romano Giannetti <romano.giannetti@gmail.com>
2014-05-15 21:40:31 -0600

    Refactor the code to follow gnome extensions suggestions
    Moved things out of init() and into enable(). We just add a
    button, which is destroyed in disable().

--- SwitchFocusType@romano.rgtti.com/extension.js
+++ SwitchFocusType@romano.rgtti.com/extension.js
@@ -14,73 +14,72 @@ const FFM=0;
 const CTF=1;
 
 function _set_FFM() {
+	button.set_child(icon_f);
 	Util.spawn(['gsettings', 'set', 'org.gnome.desktop.wm.preferences', 'focus-mode', 'sloppy']);
 }
 
 function _set_CTF() {
+	button.set_child(icon_c);
 	Util.spawn(['gsettings', 'set', 'org.gnome.desktop.wm.preferences', 'focus-mode', 'click']);
 }
 
 
 function _hideMsg() {
-    Main.uiGroup.remove_actor(text);
-    text = null;
+	Main.uiGroup.remove_actor(text);
+	text = null;
 }
 
 function _showMsg(what) {
-    if (!text) {
-        text = new St.Label({ style_class: 'msg-label', text: what });
-        Main.uiGroup.add_actor(text);
-    }
+	if (!text) {
+		text = new St.Label({ style_class: 'msg-label', text: what });
+		Main.uiGroup.add_actor(text);
+	}
 
-    text.opacity = 255;
-    let monitor = Main.layoutManager.primaryMonitor;
-    text.set_position(Math.floor(monitor.width / 2 - text.width / 2),
-                      Math.floor(monitor.height / 2 - text.height / 2));
-    Tweener.addTween(text,
-                     { opacity: 0,
-                       time: 3,
-                       transition: 'easeOutQuad',
-                       onComplete: _hideMsg });
+	text.opacity = 255;
+	let monitor = Main.layoutManager.primaryMonitor;
+	text.set_position(Math.floor(monitor.width / 2 - text.width / 2),
+			Math.floor(monitor.height / 2 - text.height / 2));
+	Tweener.addTween(text,
+			{ opacity: 0,
+				time: 3,
+		transition: 'easeOutQuad',
+		onComplete: _hideMsg });
 }
 
 function _switch() {
 	if (focus == FFM) {
-		focus = CTF;
 		_showMsg("Setting Click-to-focus");
-		button.set_child(icon_c);
+		focus = CTF;
 		_set_CTF();
-	}
-	else {
+	} else {
 		focus = FFM;
 		_showMsg("Setting Focus-follow-mouse");
-    		button.set_child(icon_f);
 		_set_FFM();
 	}
 }
 
 function init() {
-    button = new St.Bin({ style_class: 'panel-button',
-                          reactive: true,
-                          can_focus: true,
-                          x_fill: true,
-                          y_fill: false,
-                          track_hover: true });
-    Gtk.IconTheme.get_default().append_search_path(Meta.dir.get_child('icons').get_path());
-    icon_f = new St.Icon({ icon_name: 'fmode',
-                             style_class: 'system-status-icon' });
-    icon_c = new St.Icon({ icon_name: 'cmode',
-                             style_class: 'system-status-icon' });
-    button.set_child(icon_f);
-    focus = FFM;
-    _set_FFM();
-    button.connect('button-press-event', _switch);
+	button = new St.Bin({ style_class: 'panel-button',
+		reactive: true,
+	       can_focus: true,
+	       x_fill: true,
+	       y_fill: false,
+	       track_hover: true });
+	Gtk.IconTheme.get_default().append_search_path(Meta.dir.get_child('icons').get_path());
+	icon_f = new St.Icon({ icon_name: 'fmode',
+		style_class: 'system-status-icon' });
+	icon_c = new St.Icon({ icon_name: 'cmode',
+		style_class: 'system-status-icon' });
 }
 
 function enable() {
-    Main.panel._rightBox.insert_child_at_index(button, 0);
+	// start with FFM 
+	focus = FFM;
+	_set_FFM();
+	button.connect('button-press-event', _switch);
+	Main.panel._rightBox.insert_child_at_index(button, 0);
 }
 
 function disable() {
-    Main.panel._rightBox.remove_child(button);
+	Main.panel._rightBox.remove_child(button);
 }
